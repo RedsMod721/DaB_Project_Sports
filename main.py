45b2a5a857b2615803b0f7743fda527cda920976
RedsMod721 <152014793+RedsMod721@users.noreply.github.com>
2024-11-27 10:08:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from func_class import import_clean_data, Player, Team, generate_player_report, generate_team_report 
 
-data = import_clean_data("shots_2023.csv")
 shoot_data = import_clean_data("shots_2023.csv")
 team_data = pd.read_csv("skaters.csv")
 
-generate_player_report('Connor McDavid', data)
+generate_player_report('Connor McDavid', shoot_data)
 
 generate_team_report('OTT', shoot_data, team_data)
