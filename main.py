9f94daa7830a617297f0d960385ba5cf18f6a79b
RedsMod721 <152014793+RedsMod721@users.noreply.github.com>
2024-11-27 10:37:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from func_class import import_clean_data, Player, Team, generate_player_report, generate_team_report 
+import pandas as pd
 
 shoot_data = import_clean_data("shots_2023.csv")
 team_data = pd.read_csv("skaters.csv")
